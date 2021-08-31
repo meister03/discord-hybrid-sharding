01d193013a66533c832ba739cf53f92b46106b43
infinitytmbots <69136448+infinitytmbots@users.noreply.github.com>
2021-08-31 19:16:00 +0530

    Revert "Update Cluster.js"
    This reverts commit 92177ab2080aab3a65d4b265638bed5888047207.

--- src/Cluster.js
+++ src/Cluster.js
@@ -145,7 +145,7 @@ class Cluster extends EventEmitter {
     this.emit('spawn', this.process || this.worker);
 
     if (spawnTimeout === -1 || spawnTimeout === Infinity) return this.process || this.worker;
-    spawnTimeout = spawnTimeout*this.shardlist.length;
+    spawnTimeout = spawnTimeout*this.shardlist;
     await new Promise((resolve, reject) => {
       const cleanup = () => {
         clearTimeout(spawnTimeoutTimer);
