349c73222100c06929b30d91cf602f9b8a597e4d
Callum OKane <35779365+callumok2004@users.noreply.github.com>
2021-12-31 21:27:53 +0000

    Update typings.d.ts

--- typings.d.ts
+++ typings.d.ts
@@ -90,7 +90,7 @@ declare module 'discord-hybrid-sharding' {
       options?: {
         totalShards?: number | 'auto';
         totalClusters?: number | 'auto';
-        shardsPerClusters?: number | 'auto';
+        shardsPerClusters?: number;
         shardList?: number[] | 'auto';
         mode?: ClusterManagerMode;
         respawn?: boolean;
