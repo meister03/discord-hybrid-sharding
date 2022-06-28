c58a81e87061a2e574824f4898bf3119aa30ccd0
meister03 <meisterpi@gmail.com>
2022-06-28 16:48:34 +0200

    Update Docs and Bump Version

--- docs/scripts/search.js
+++ docs/scripts/search.js
@@ -1,77 +1,279 @@
 /* global document */
-function hideSearchList() {
-    document.getElementById('search-item-ul').style.display = 'none';
-}
 
-function showSearchList() {
-    document.getElementById('search-item-ul').style.display = 'block';
-}
+var searchData;
+var searchId = 'LiBfqbJVcV';
+var searchHash = '#' + searchId;
+var searchContainerID = '#PkfLWpAbet';
+var searchWrapperID = '#iCxFxjkHbP';
+var searchCloseButtonID = '#VjLlGakifb';
+var searchInputID = '#vpcKVYIppa';
+var searchResultCID = '#fWwVHRuDuN';
 
-function checkClick(e) {
-    if ( e.target.id !== 'search-box-input') {
-        setTimeout(function() {
-            hideSearchList();
-        }, 60);
+function hideSearch() {
+  var container = document.querySelector(searchContainerID);
 
-        /* eslint-disable-next-line */
-        window.removeEventListener('click', checkClick);
-    }
-}
-
-function search(list, options, keys, searchKey) {
-    var defaultOptions = {
-        shouldSort: true,
-        threshold: 0.4,
-        location: 0,
-        distance: 100,
-        maxPatternLength: 32,
-        minMatchCharLength: 1,
-        keys: keys
-    };
-
-    var op = Object.assign({}, defaultOptions, options);
-
+  // eslint-disable-next-line no-undef
+  if (window.location.hash === searchHash) {
     // eslint-disable-next-line no-undef
-    var searchIndex = Fuse.createIndex(op.keys, list);
+    history.go(-1);
+  }
 
-    /* eslint-disable-next-line */
+  // eslint-disable-next-line no-undef
+  window.onhashchange = null;
+
+  if (container) {
+    container.style.display = 'none';
+  }
+}
+
+function listenKey(event) {
+  if (event.key === 'Escape') {
+    hideSearch();
+    // eslint-disable-next-line no-undef
+    window.removeEventListener('keyup', listenKey);
+  }
+}
+
+function showSearch() {
+  var container = document.querySelector(searchContainerID);
+  var input = document.querySelector(searchInputID);
+
+  // eslint-disable-next-line no-undef
+  window.onhashchange = hideSearch;
+
+  // eslint-disable-next-line no-undef
+  if (window.location.hash !== searchHash) {
+    // eslint-disable-next-line no-undef
+    history.pushState(null, null, searchHash);
+  }
+
+  if (container) {
+    container.style.display = 'flex';
+    // eslint-disable-next-line no-undef
+    window.addEventListener('keyup', listenKey);
+  }
+
+  if (input) {
+    input.focus();
+  }
+}
+
+function fetchAllData(obj = {}) {
+  // eslint-disable-next-line no-undef
+  var url = baseURL + 'data/search.json';
+
+  fetch(url)
+    .then(function(d) {
+      return d.json();
+    })
+    .then(function(d) {
+      searchData = d.list;
+      if (typeof obj.onSuccess === 'function') {
+        obj.onSuccess(d.list);
+      }
+    })
+    .catch(function(error) {
+      console.error(error);
+      if (typeof obj.onError === 'function') {
+        obj.onError();
+      }
+    });
+}
+
+// eslint-disable-next-line no-unused-vars
+function onClickSearchItem(event) {
+  var target = event.currentTarget;
+
+  if (target) {
+    var href = target.getAttribute('href') || '';
+    var id = href.split('#')[1] || '';
+    var element = document.getElementById(id);
+
+    if (!element) {
+      id = decodeURI(id);
+      element = document.getElementById(id);
+    }
+
+    if (element) {
+      setTimeout(function() {
+        // eslint-disable-next-line no-undef
+        bringElementIntoView(element); // defined in core.js
+      }, 100);
+    }
+  }
+}
+
+function buildSearchResult(result) {
+  var output = '';
+
+  for (const res of result) {
+    var data = res.item;
+
+    var link = res.item.link.replace('<a href="', '').replace(/">.*/, '');
+
+    output += `
+
+    <a onclick="onClickSearchItem(event)" href="${link}" class="search-result-item">
+      <div class="search-result-item-title">
+          ${data.title}
+      </div>
+      <div class="search-result-item-p">
+          ${data.description ? data.description : 'No description available.'}
+      </div>
+    </a>
+    `;
+  }
+
+  return output;
+}
+
+function getSearchResult(list, keys, searchKey) {
+  var defaultOptions = {
+    shouldSort: true,
+    threshold: 0.4,
+    location: 0,
+    distance: 100,
+    maxPatternLength: 32,
+    minMatchCharLength: 1,
+    keys: keys
+  };
+
+  // var op = Object.assign({}, defaultOptions, options);
+  var op = defaultOptions;
+
+  // eslint-disable-next-line no-undef
+  var searchIndex = Fuse.createIndex(op.keys, list);
+
+  /* eslint-disable-next-line */
     var fuse = new Fuse(list, op, searchIndex);
 
-    var result = fuse.search(searchKey);
+  var result = fuse.search(searchKey);
 
-    if (result.length > 20) { result = result.slice(0, 20); }
+  if (result.length > 20) {
+    result = result.slice(0, 20);
+  }
 
-    var searchUL = document.getElementById('search-item-ul');
+  return result;
+}
 
-    if (result.length === 0) {
-        searchUL.innerHTML = '<li class="p-h-n"> No Result Found </li>';
-    } else {
-        searchUL.innerHTML = result.reduce(function(html, obj) {
-            return html + '<li>' + obj.item.link + '</li>';
-        }, '');
+function debounce(func, wait, immediate) {
+  var timeout;
+
+  return function() {
+    // eslint-disable-next-line consistent-this, no-invalid-this
+    var context = this,
+      args = arguments;
+
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      timeout = null;
+      if (!immediate) {
+        func.apply(context, args);
+      }
+    }, wait);
+    if (immediate && !timeout) {
+      func.apply(context, args);
     }
+  };
 }
 
-/* eslint-disable-next-line */
-function setupSearch(list, options) {
-    var inputBox = document.getElementById('search-box-input');
-    var keys = ['title'];
+function search(event) {
+  var value = event.target.value;
+  var resultBox = document.querySelector(searchResultCID);
+  var keys = ['title', 'description'];
 
-    inputBox.addEventListener('keyup', function() {
-        if (inputBox.value !== '') {
-            showSearchList();
-            search(list, options, keys, inputBox.value);
-        }
-        else { hideSearchList(); }
+  if (!resultBox) {
+    console.error('Search result container not found');
+
+    return;
+  }
+
+  if (!value) {
+    resultBox.innerHTML = 'Type anything to view search result';
+
+    return;
+  }
+
+  function onSuccess(res) {
+    if (res.length === 0) {
+      resultBox.innerHTML =
+                'No result found! Try some different combination.';
+
+      return;
+    }
+    var output = buildSearchResult(res);
+
+    resultBox.innerHTML = output;
+  }
+
+  if (!searchData) {
+    resultBox.innerHTML = 'Loading...';
+
+    fetchAllData({
+      onSuccess: function(list) {
+        var result = getSearchResult(list, keys, value);
+
+        onSuccess(result);
+      },
+      onError: function() {
+        resultBox.innerHTML = 'Failed to load result.';
+      }
     });
 
-    inputBox.addEventListener('focus', function() {
-        showSearchList();
-        if (inputBox.value !== '') {
-            search(list, options, keys, inputBox.value);
-        }
+    return;
+  }
 
-        /* eslint-disable-next-line */
-        window.addEventListener('click', checkClick);
-    });
+  var result = getSearchResult(searchData, keys, value);
+
+  onSuccess(result);
 }
+
+function onDomContentLoaded() {
+  var input = document.querySelector(searchInputID);
+  var searchButton = document.querySelectorAll('.search-button');
+  var searchContainer = document.querySelector(searchContainerID);
+  var searchWrapper = document.querySelector(searchWrapperID);
+  var searchCloseButton = document.querySelector(searchCloseButtonID);
+
+  var debouncedSearch = debounce(search, 300);
+
+  if (searchCloseButton) {
+    searchCloseButton.addEventListener('click', hideSearch);
+  }
+
+  if (searchButton) {
+    searchButton.forEach(function(item) {
+      item.addEventListener('click', showSearch);
+    });
+  }
+
+  if (searchContainer) {
+    searchContainer.addEventListener('click', hideSearch);
+  }
+
+  if (searchWrapper) {
+    searchWrapper.addEventListener('click', function(event) {
+      event.stopPropagation();
+    });
+  }
+
+  if (input) {
+    input.addEventListener('keyup', debouncedSearch);
+  }
+
+  // eslint-disable-next-line no-undef
+  if (window.location.hash === searchHash) {
+    showSearch();
+  }
+}
+
+// eslint-disable-next-line no-undef
+window.addEventListener('DOMContentLoaded', onDomContentLoaded);
+
+// eslint-disable-next-line no-undef
+window.addEventListener('hashchange', function() {
+  // eslint-disable-next-line no-undef
+  if (window.location.hash === searchHash) {
+    showSearch();
+  }
+});
--- src/Plugins/ReCluster.js
+++ src/Plugins/ReCluster.js
@@ -6,7 +6,7 @@ class ReClusterManager {
         this.name = 'recluster';
         this.onProgress = false;
     }
-    build(manager){
+    build(manager) {
         manager[this.name] = this;
         this.manager = manager;
         return this;
@@ -23,26 +23,38 @@ class ReClusterManager {
      * @param {?number[][]} options.shardClusterList The shardList chunked over the clusters
      * @param {?number[]} options.shardList The new shardList of the Cluster Manager
      * @param {?string} options.restartMode The restartMode of the clusterManager, gracefulSwitch = waits until all new clusters have spawned with maintenance mode, rolling = Once the Cluster is Ready, the old cluster will be killed
-     * @returns
+     * @returns {ReClusterManager._start}
      */
-    async start({delay, timeout, totalShards, totalClusters, shardsPerClusters, shardClusterList, shardList = this.manager.shardList, restartMode = 'gracefulSwitch'}){
-        if(this.onProgress) throw new Error('Zero Downtime Reclustering is already in progress');
-        if(totalShards){
-            if(totalShards === 'auto') totalShards = await Util.fetchRecommendedShards(this.manager.token);
+    async start({
+        delay,
+        timeout,
+        totalShards,
+        totalClusters,
+        shardsPerClusters,
+        shardClusterList,
+        shardList = this.manager.shardList,
+        restartMode = 'gracefulSwitch',
+    }) {
+        if (this.onProgress) throw new Error('Zero Downtime Reclustering is already in progress');
+        if (totalShards) {
+            if (totalShards === 'auto') totalShards = await Util.fetchRecommendedShards(this.manager.token);
             this.manager.totalShards = totalShards;
         }
-        console.log(totalClusters)
-        if(totalClusters) this.manager.totalClusters = totalClusters;
-        console.log(this.manager.totalClusters)
-        if(shardsPerClusters) {
+        console.log(totalClusters);
+        if (totalClusters) this.manager.totalClusters = totalClusters;
+        console.log(this.manager.totalClusters);
+        if (shardsPerClusters) {
             this.manager.shardsPerClusters = shardsPerClusters;
             this.manager.totalClusters = Math.ceil(this.manager.totalShards / this.manager.shardsPerClusters);
         }
 
-        if(shardList) this.manager.shardList = shardList;
+        if (shardList) this.manager.shardList = shardList;
         else this.manager.shardList = [...Array(this.manager.totalShards).keys()];
-        if(shardClusterList) this.manager.shardClusterList = shardClusterList;
-        else this.manager.shardClusterList = this.manager.shardList?.chunk(Math.ceil(this.manager.shardList.length / this.manager.totalClusters));
+        if (shardClusterList) this.manager.shardClusterList = shardClusterList;
+        else
+            this.manager.shardClusterList = this.manager.shardList?.chunk(
+                Math.ceil(this.manager.shardList.length / this.manager.totalClusters),
+            );
 
         if (this.manager.shardClusterList.length !== this.manager.totalClusters) {
             this.manager.totalClusters = this.manager.shardClusterList.length;
@@ -56,12 +68,19 @@ class ReClusterManager {
                 `├── Shards Per Cluster: ${this.manager.shardsPerClusters}`,
                 `├── Shard Cluster List: ${this.manager.shardClusterList.join(', ')}`,
                 `└── Shard List: ${this.manager.shardList.join(', ')}`,
-            ].join('\n')
-        )
-        return this._start({restartMode, timeout, delay});
+            ].join('\n'),
+        );
+        return this._start({ restartMode, timeout, delay });
     }
 
-    async _start({restartMode, timeout = 30000*6 , delay = 7000}){
+    /**
+     * @param {object} options
+     * @param {?number} options.delay The delay to wait between each cluster spawn
+     * @param {?number} options.timeout The readyTimeout to wait until the cluster spawn promise is rejected
+     * @param {?string} options.restartMode The restartMode of the clusterManager, gracefulSwitch = waits until all new clusters have spawned with maintenance mode, rolling = Once the Cluster is Ready, the old cluster will be killed
+     * @returns {Promise<Object>}
+     */
+    async _start({ restartMode, timeout = 30000 * 6, delay = 7000 }) {
         process.env.MAINTENANCE = 'recluster';
         this.manager.triggerMaintenance('recluster');
         this.manager._debug('[↻][ReClustering] Enabling Maintenance Mode on all clusters');
@@ -81,22 +100,27 @@ class ReClusterManager {
             const spawnDelay = delay * this.manager.shardClusterList[i].length;
             this.manager.queue.add({
                 run: (...a) => {
-                    const cluster = this.manager.createCluster(clusterId, this.manager.shardClusterList[i], this.manager.totalShards, true);
+                    const cluster = this.manager.createCluster(
+                        clusterId,
+                        this.manager.shardClusterList[i],
+                        this.manager.totalShards,
+                        true,
+                    );
                     newClusters.set(clusterId, cluster);
                     this.manager._debug(`[↻][ReClustering][${clusterId}] Spawning... Cluster`);
-                    return cluster
-                    .spawn(...a)
-                    .then((c) => {
+                    return cluster.spawn(...a).then(c => {
                         this.manager._debug(`[↻][ReClustering][${clusterId}] Cluster Ready`);
-                        if(switchClusterAfterReady) {
+                        if (switchClusterAfterReady) {
                             const oldCluster = this.manager.clusters.get(clusterId);
-                            if(oldCluster){
-                                oldCluster.kill({force: true});
+                            if (oldCluster) {
+                                oldCluster.kill({ force: true });
                                 oldClusters.delete(clusterId);
                             }
                             this.manager.clusters.set(clusterId, cluster);
                             cluster.triggerMaintenance();
-                            this.manager._debug(`[↻][ReClustering][${clusterId}] Switched OldCluster to NewCluster and exited Maintenance Mode`);
+                            this.manager._debug(
+                                `[↻][ReClustering][${clusterId}] Switched OldCluster to NewCluster and exited Maintenance Mode`,
+                            );
                         }
                         return c;
                     });
@@ -106,28 +130,32 @@ class ReClusterManager {
             });
         }
         await this.manager.queue.start();
-        if(oldClusters.size){
+        if (oldClusters.size) {
             this.manager._debug('[↻][ReClustering] Killing old clusters');
             for (let [id, cluster] of oldClusters) {
-                cluster.kill({force: true});
+                cluster.kill({ force: true });
                 this.manager._debug(`[↻][ReClustering][${id}] Killed OldCluster`);
                 this.manager.clusters.delete(id);
             }
             oldClusters.clear();
         }
-        if(!switchClusterAfterReady) {
-            this.manager._debug('[↻][ReClustering] Starting exiting Maintenance Mode on all clusters and killing old clusters');
+        if (!switchClusterAfterReady) {
+            this.manager._debug(
+                '[↻][ReClustering] Starting exiting Maintenance Mode on all clusters and killing old clusters',
+            );
             for (let i = 0; i < this.manager.totalClusters; i++) {
                 const clusterId = this.manager.clusterList[i] || i;
                 const cluster = newClusters.get(clusterId);
                 const oldCluster = this.manager.clusters.get(clusterId);
-                if(oldCluster) {
-                    oldCluster.kill({force: true});
+                if (oldCluster) {
+                    oldCluster.kill({ force: true });
                     oldClusters.delete(clusterId);
                 }
                 this.manager.clusters.set(clusterId, cluster);
                 cluster.triggerMaintenance();
-                this.manager._debug(`[↻][ReClustering][${clusterId}] Switched OldCluster to NewCluster and exited Maintenance Mode`);
+                this.manager._debug(
+                    `[↻][ReClustering][${clusterId}] Switched OldCluster to NewCluster and exited Maintenance Mode`,
+                );
             }
         }
 
@@ -135,7 +163,7 @@ class ReClusterManager {
         this.onProgress = false;
         process.env.MAINTENANCE = undefined;
         this.manager._debug('[↻][ReClustering] Finished ReClustering');
-        return {success: true};
+        return { success: true };
     }
 }
 module.exports = ReClusterManager;
